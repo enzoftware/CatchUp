3f32692912f9f73a6fac2b117b7d235d63dc0700
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2018-09-06 21:32:31 -0500

    Showing :recycle:View

--- app/src/main/java/com/upc/projects/enzoftware/catchup/ui/fragments/SourcesFragment.kt
+++ app/src/main/java/com/upc/projects/enzoftware/catchup/ui/fragments/SourcesFragment.kt
@@ -3,28 +3,28 @@ package com.upc.projects.enzoftware.catchup.ui.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.upc.projects.enzoftware.catchup.R
+import com.upc.projects.enzoftware.catchup.model.Source
+import com.upc.projects.enzoftware.catchup.ui.adapters.SourcesAdapter
+import kotlinx.android.synthetic.main.fragment_sources.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class SourcesFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sources, container, false)
+        val view = inflater.inflate(R.layout.fragment_sources, container, false)
+        val sourcesRecyclerView = view.sourcesRecyclerView
+        val sourcesAdapter = SourcesAdapter(ArrayList<Source>(), view.context)
+        val sourcesLayoutManager = GridLayoutManager(view.context, 2)
+        sourcesRecyclerView.adapter = sourcesAdapter
+        sourcesRecyclerView.layoutManager = sourcesLayoutManager
+        return view
     }
-
 
 }
