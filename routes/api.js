4b9ef36db5bfbe03bdecb58b0ab280584055a17e
enzoftware <lizama.enzo@gmail.com>
2019-01-08 01:14:09 -0500

    Products endpoints

--- routes/api.js
+++ routes/api.js
@@ -52,7 +52,34 @@ router.post('/login', function(req, res) {
         .catch((error) => res.status(400).send(error));
 });
 
-
+router.get('/product', passport.authenticate('jwt', { session: false}), function(req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+      Product
+        .findAll()
+        .then((products) => res.status(200).send(products))
+        .catch((error) => { res.status(400).send(error); });
+    } else {
+      return res.status(403).send({success: false, msg: 'Unauthorized.'});
+    }
+  });
+  
+  router.post('/product', passport.authenticate('jwt', { session: false}), function(req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+      Product
+        .create({
+          prod_name: req.body.prod_name,
+          prod_desc: req.body.prod_desc,
+          prod_price: req.body.prod_price
+        })
+        .then((product) => res.status(201).send(product))
+        .catch((error) => res.status(400).send(error));
+    } else {
+      return res.status(403).send({success: false, msg: 'Unauthorized.'});
+    }
+  });
+  
 
 
 getToken = function(headers) {
