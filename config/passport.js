15fa4d69576dea1bb8174fad83293a6a644af9e0
enzoftware <lizama.enzo@gmail.com>
2019-01-08 00:22:25 -0500

    Adding passport.js security to routes.js

--- config/passport.js
+++ config/passport.js
@@ -3,12 +3,15 @@ const jwtExtract    = require('passport-jwt').ExtractJwt;
 
 const User          = require('../models').User;
 
-module.exports = function(passport) {
-    
+module.exports = function(passport) {    
     const opts = {
-
+        jwtFromRequest: jwtExtract.fromAuthHeaderWithScheme('JWT'),
+        secretOrKey: 'nodeauthsecret',
     };
-    
-
-
+    passport.use('jwt', new jwtStrategy(opts, function(jwt_payload, done) {
+        User
+            .findByPk(jwt_payload.id)
+            .then((user) => {return done(null, user);})
+            .catch((error) => {return done(error, false);});
+    }));
 };
